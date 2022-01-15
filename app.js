ba28c4198995f1a57e40bc5fde0acdf90f0f3665
CharaSp <56803051+CharaSp@users.noreply.github.com>
2022-01-15 16:01:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,228 @@
+var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+const bodyParser = require('body-parser');
+var app = express();
+const mysql = require('mysql2/promise');
+const { promisePool } = require('./config/db_connect');
+const fileupload = require('express-fileupload');
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views')
+app.use('/public', express.static(path.join(__dirname, "public")));
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileupload());
+app.use(express.json());
+app.use(bodyParser());
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("running on port 3000");
+})
+
+
+
+
+
+app.get('/', function (req, res) {
+    res.render('login.ejs')
+})
+
+
+app.get('/signup', function (req, res) {
+    res.render('signup.ejs')
+})
+
+
+app.get('/mainpage', function (req, res) {
+    res.render('mainpage.ejs')
+})
+
+app.get('/editusrprofile', function (req, res) {
+    res.render('edit_usr_profile.ejs')
+})
+
+
+app.get('/userhistory', function (req, res) {
+    res.render('usr_history.ejs')
+})
+
+app.get('/admin', function (req, res) {
+    res.render('adminaddst.ejs')
+})
+
+
+
+app.post('/test', function (req, res) {
+
+    console.log(req.body)
+
+    const myVal = req.body.file
+    
+
+    
+    res.send(myVal)
+
+})
+
+
+//app.get('/mainpage', function (req, res) {
+
+ //   res.render('mainpage.ejs')
+//})
+
+app.get('/sentdata', function (req, res) {
+
+})
+
+app.post('/sentdata', function (req, res) {
+    console.log(req.body);
+    //oles tis plirofories keimenoy poy stelnw
+    //prepei na psaxw th vash na vrw poies topothesies exoun type auto poy egrapse o xristis
+    //ti egrapse omws o xroistis?
+    const usersearchFor = req.body.writtenData
+     
+})
+
+app.post('/', async function (req, res) {
+    const username = req.body.user;
+
+    const password = req.body.pass;
+
+
+
+    const sqlQeuryString = `SELECT * FROM user WHERE username='${username}' AND password='${password}'`;
+    const dataBaseUsersResponse = await promisePool.query(sqlQeuryString);
+    // console.log(dataBaseUsersResponse[0]);
+
+
+
+    if (dataBaseUsersResponse[0].length > 0) {
+        res.send('success')
+        console.log('you made it')
+    }
+    else {
+        res.send('fail')
+        console.log('BYEE')
+    }
+
+    // const sqlQeuryString = `INSERT INTO user(\`username\`, \`password\`, \`email\`) VALUES ('${username}', '${password}', '${email}') `; 
+})
+
+
+app.post('/uploadjson', async function (req, res) {
+    //1 na anevoun ta arxeia ston server
+    //2 na analuyseis to json opos kaname sto arxeio jsobject.js
+    //3 na ta peraseis sthn vash
+
+    //1
+    const myFiles = req.files.myFile;
+        const fileData = myFiles.data;
+
+    // pleon edw exoume diavasei to arxeio
+    const getFileJsonData = JSON.parse(fileData);
+    // console.log(getFlieJsonData);
+
+
+    // edw kovoume apo thn ka8e eggrafi kai kratame mono ta dedomena pou 9eloume..
+    const allPlaces = getFileJsonData.map(place => {
+        return {
+            coοrdinates: place.coordinates, // kratame tin topo9esia
+            name: place.name, // to onoma
+            types: place.types, // to types
+            populartimes: place.populartimes, // 
+            id: place.id
+        }
+    });
+
+    console.log(allPlaces);
+
+
+
+    // prepei na peraseis ta dedomena sthn vash...
+
+    // ta dedomena pleon vriskontai ston pinaka allPlaces..
+    allPlaces.map(async (place) => {
+        // ftiaxnw to query
+        const sql = `INSERT INTO PLACE (name, coordinates, types, populartimes) 
+        VALUES('${place.name}', Point(${place.coordinates.lat}, ${place.cordinates.lng}), '${JSON.stringify(place.types)}', '${JSON.stringify(place.populartimes)}' )`;
+        // console.log(sql);
+        let response;
+        // εκτελεσε το query
+        try {
+
+            response = await promisePool.query(sql)
+        } catch (error) {
+            console.log('cant insert,', error);
+        }
+    })
+})
+
+app.post('/signup', function(req, res) {
+    console.log(req.body);
+
+    console.log(req.body.userinfo);
+})
+
+/*app.post('/uploadjson',async function(res,req){
+    
+    const myFiles=req.files.myFiles;
+    const fileData= myFiles.data;
+
+    const getFlieJsonData = JSON.parse(fileData);
+    // console.log(getFlieJsonData);
+
+    //2:
+
+    const allPlaces = getFlieJsonData.map(place => {
+        return {
+            cordinates: place.coordinates,
+            name: place.name,
+            types: place.types,
+            populartimes: place.populartimes,
+            id: place.id
+        }
+
+    })
+
+
+    // 3:
+    // gia ka8e place (dld gia ka8e stoixieio tou pinaka)
+    allPlaces.map(place => {
+        // ftiaxnw to query
+        const sql = `INSERT INTO PLACE (name, cordinates, types, populartimes) 
+        VALUES('${place.name}', Point(${place.cordinates.lat}, ${place.cordinates.lng}), '${JSON.stringify(place.types)}', '${JSON.stringify(place.populartimes)}' )`;
+
+
+        promisePool.query(sql)
+
+
+        // // // DEUTERH POROSEGGISI
+        // console.log(place.populartimes);
+        // place.populartimes.map(pt => {
+        //     const sql = `INSERT INTO populartimes (name, h00, h01,h02...) VALUES(${pt.name}, ${pt.data[0]}, ${pt.data[1]}, ${pt.data[2]})`
+        // })
+
+
+
+
+        //milaw me thn vash
+    })
+    // console.log(allPlaces);
+
+    res.send('ok')
+})
+
+*/
+
+
+
+
+
