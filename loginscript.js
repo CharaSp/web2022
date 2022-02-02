e61c4aba37bbac08e0166b80845907d47b8d4068
XristinaSyk <65867825+XristinaSyk@users.noreply.github.com>
2022-02-02 13:41:16 +0200

    Update loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -1,12 +1,30 @@
 async function loginuser() {
-    let username = document.getElementById('exampleInputEmail1').value;
-    let password = document.getElementById('exampleInputPassword1').value;
+    let username = document.getElementById("usernemail").value;
+    let password = document.getElementById("examplepassword").value;
 
     let info = {
         user: username,
         pass: password
     }
 
-    let response = await axios.get('/userlogin', info)
-    console.log(response)
+    let response = await axios.post('/userlogin', info)
+    console.log(response);
+
+    if (response.data == 'valid') {
+        window.location.href = 'http://localhost:3000/mainpage';
+    } else {
+        alert('Λάθος στοιχεία χρήστη');
+        window.location.href = 'http://localhost:3000/';
+    }
+
+    //alert("trexei");
+
+
+    // async function useraccess(){
+    //     let response1= await axios.get('/userlogin')
+    //     console.log(response1)
+    // }
+
+    // window.location.href = 'http://localhost:3000/mainpage'
+
 }
