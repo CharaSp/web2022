47f0838b742fb734e45125841ca0056be1df035b
CharaSp <56803051+CharaSp@users.noreply.github.com>
2022-02-05 11:53:06 +0200

    Add files via upload

--- code.sql
+++ code.sql
@@ -4,13 +4,12 @@
     'firstname' varchar(100) NOT NULL,
     'lastname' varchar(100) NOT NULL,
     `userId` INT(10) AUTO_INCREMENT ,
-    `email` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL unique,
     'dateofbirth' DATE ,
     `admin` TINYINT(1) NOT NULL,
     PRIMARY KEY (`userId`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-  
   CREATE table 'place' (
     `placeId` INT(10) AUTO_INCREMENT ,
     `name` varchar(100) NOT NULL unique,
@@ -21,4 +20,13 @@
     `populartimes` JSON NOT NULL,
     `types` JSON NOT NULL,
       PRIMARY KEY (`placeId`)
-  )ENGINE=InnoDB DEFAULT CHARSET=utf8
+  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+  CREATE TABLE `userhistory` (
+    `userId` INT(10),
+    `userpositive` varchar(10),
+    `usernegative` varchar(10),
+    `dateofcovid` DATE,
+    `placevisitedday` DATE,
+    FOREIGN KEY (`userId`) REFERENCES user(userId)
+  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
