1950031e4c54b2916937894d6e170bfd1c6e7476
CharaSp <56803051+CharaSp@users.noreply.github.com>
2022-02-06 18:36:42 +0200

    Update mainpscrpt.js

--- mainpscrpt.js
+++ mainpscrpt.js
@@ -1,6 +1,6 @@
 //const Connection = require("mysql2/typings/mysql/lib/Connection");
 //για να αρχικοποιησεις ενα χαρτη _firstLatLng,_firstPoint,_secondLatLng,_secondPoint,_distance,_length,_polyline,
-    var map = L.map('my_map').setView([38.245865, 21.732860], 5);
+    var map = L.map('my_map').setView([38.245865,21.732860], 5);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -18,14 +18,14 @@
 
     function success(position) {
         console.log(position);
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
 
         console.log(lat, lng);
 
         const m1 = L.marker([lat, lng]);
 
-        m1.bindPopup('<p><b> I\'m Here!</b> </p> <button>I\'m here </button>')
+        m1.bindPopup('<p><b> I\'m Here!</b> </p> <button id="loccationbutton" onclick="distance()">I\'m here <input id="cb2" type="checkbox" /></button>')
         m1.addTo(map)
         map.flyTo([lat, lng], 17)
         
@@ -36,7 +36,16 @@
             opacity: 0.5 
         }).addTo(map)
 
+        return lat,lng;
+
     }
+
+
+    
+    /*async function userloc(){
+        if()
+        alert("im here!");
+    }*/
 
     function fail() {
         alert('failed tracing you... reload');
@@ -46,7 +55,6 @@
     //gia thn paron topo8esia sou otan sumdeesai uparxei: to navigator  https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition?retiredLocale=el
     //αναλογα αν πετυχει η διαδικασια εντοπισμου του χρηστη τρεξε mia συναρτηση allios 8a trexei thn allh sunartisi 
     navigator.geolocation.getCurrentPosition(success, fail, options);
-
 
 async function userSearch() {
     
@@ -85,20 +93,58 @@ async function userSearch() {
     //console.log(usersearches.srchbar);
     let response3 = await axios.post('/usersearch', usersearches);
     //console.log(response3);
-    const data2 = response3.data;
+    var data2 = response3.data;
     console.log(data2);
     const today = new Date();
     var tomorrow = new Date();
     tomorrow.setDate(new Date().getDate()+1);
 
+
     for(let i=0; i<data2.length; i++)
     {    
          let poptypes=JSON.parse(data2[i].populartimes);
-         var myMarker1 = L.marker([data2[i].coordinates.x,data2[i].coordinates.y],{icon:leafletIcon1}); 
-         var myMarker2 = L.marker([data2[i].coordinates.x,data2[i].coordinates.y],{icon:leafletIcon2});       
-         var myMarker3 = L.marker([data2[i].coordinates.x,data2[i].coordinates.y],{icon:leafletIcon3});       
-      
+         var myMarker1 = L.marker([data2[i].coordinates.x,data2[i].coordinates.y],{icon:leafletIcon1}).on('click', onClick); 
+         var myMarker2 = L.marker([data2[i].coordinates.x,data2[i].coordinates.y],{icon:leafletIcon2}).on('click', onClick);       
+         var myMarker3 = L.marker([data2[i].coordinates.x,data2[i].coordinates.y],{icon:leafletIcon3}).on('click', onClick);       
+         //map.on('click', onClick);
         
+         async function onClick(){
+            let placecoords=this.getLatLng();
+            console.log(placecoords);
+            let latid2=placecoords.lat;
+            let long2=placecoords.lng;
+            var myposition=await getPosition();
+            var latid1=myposition.coords.latitude;
+            var long1=myposition.coords.longitude;
+            var distance=calcCrow(38.250065,21.738007,latid2,long2).toFixed(1);
+            console.log(distance + "km ");
+            
+            if (distance<=0.02){
+                //window.confirm("Έχετε δυνατοτητα καταχωρησης της επισκεψης σας. Αν πατήσετε οκ συναινείται στην αποθήκευση της τοποθεσίας σας.")
+               if(confirm("Έχετε δυνατοτητα καταχωρησης της επισκεψης σας. Αν πατήσετε οκ συναινείται στην αποθήκευση της τοποθεσίας σας."))
+               {//(confirm("You pressed OK!"))
+                    var useragrees='true';
+        
+                    let userwants={
+                        useriscool: useragrees,
+                        userslat: latid2,
+                        userslong: long2
+                    }            
+                    //console.log(userwants);
+                     let response4 = await axios.post('/usercool', userwants);
+                     var data3=response4.data;
+                     console.log(data3);
+        
+                     //if(data3==='userok')
+        
+                } else {
+                    txt = "Δεν θα γίνει καταψώρηση των στοιχείων σας!";
+                }
+            }   
+        }
+
+        var distance=calcCrow(38.250065,21.738007,data2[i].coordinates.x,data2[i].coordinates.y).toFixed(1);
+
          //console.log(today);
          //obj me ta name poptimes
          const myday=poptypes.find(ele=>ele.name==today.toLocaleString('en-us', {weekday:'long'}));
@@ -118,21 +164,90 @@ async function userSearch() {
              nexthour1=nextday.data[k];
              nexthour2=nextday.data[k+1];
          }
-
-        if(nexthour1<33){
-            myMarker1.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2} %</div> Καταχωρήστε εκτιμώμενο αριθμό ατόμων:`);
-            myMarker1.addTo(map);  
-        }
-        else if(nexthour1> 32 && nexthour1<65){
-            myMarker2.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2} %</div> Καταχωρήστε εκτιμώμενο αριθμό ατόμων: `);
-            myMarker2.addTo(map);  
-        }
-        else if(nexthour1>65){
-            myMarker3.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2} %</div> Καταχωρήστε εκτιμώμενο αριθμό ατόμων:`);
-            myMarker3.addTo(map);  
-        }
+       
+        var distnnum=parseInt(distance)
+        //console.log(distnnum);
+         if (distnnum<=0.02)
+        {
+            if(nexthour1<33){
+                myMarker1.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Μέσος αριθμός επισκεπτών τις επόμενες 2 ώρες: <br> ${nexthour1}%,${nexthour2}% <br> Καταχωρήστε εκτιμώμενο αριθμό ατόμων: <input type="number" min="0" max="200"class="login__input" id="peoplenum" placeholder="Αριθμός ατόμων τώρα"></div>`);
+                myMarker1.addTo(map);  
+            }
+            else if(nexthour1> 32 && nexthour1<65){
+                myMarker2.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2}% <br> Καταχωρήστε εκτιμώμενο αριθμό ατόμων: <input type="number" min="0" max="200"class="login__input" id="peoplenum" placeholder="Αριθμός ατόμων τώρα"></div>`);
+                myMarker2.addTo(map);  
+            }
+            else if(nexthour1>=65){
+                myMarker3.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2}% <br>Καταχωρήστε εκτιμώμενο αριθμό ατόμων: <input type="number" min="0" max="200"class="login__input" id="peoplenum" placeholder="Αριθμός ατόμων τώρα"></div>`);
+                myMarker3.addTo(map);  
+            }
+         } 
+       else if( distnnum>0.02)
+        {
+             if(nexthour1<33){
+                 myMarker1.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Μέσος αριθμός επισκεπτών τις επόμενες 2 ώρες: <br>  ${nexthour1}%,${nexthour2} %</div>`);
+                 myMarker1.addTo(map);  
+             }
+             else if(nexthour1> 32 && nexthour1<65){
+                 myMarker2.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2} %</div> `);
+                 myMarker2.addTo(map);  
+             }
+             else if(nexthour1>=65){
+                 myMarker3.bindPopup(`<div><b> ${data2[i].name}</b><br> ${currhour}<br> Εκτιμώμενη Προσέλευση τις επόμενες 2 ώρες:<br> ${nexthour1}%,${nexthour2} %</div>`);
+                 myMarker3.addTo(map);  
+             }
+         }
     }
 }
+
+
+
+    //return coordins;
+    //console.log(latid2,long2);
+    // console.log(latid2,long2);
+
+
+/*async function distance(){
+    if()
+}*/
+
+function calcCrow(lat1, lon1, lat2, lon2) 
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
+
+function getPosition(options){
+    return new Promise((resolve, reject) => 
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+    );
+}
+
+// async function distance(){
+//     var myposition=await getPosition();
+//     var latid1=myposition.coords.latitude;
+//     var long1=myposition.coords.longitude;
+//     // var name1=success();
+//     var name2=onClick();
+//     console.log(name2);
+//     //console.log(calcCrow(latid1,long1,latid2,long2).toFixed(1));
+// }
+
 
 async function userlogout() {
 
