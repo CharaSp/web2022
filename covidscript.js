fcc2103ce321f638fa8b60b14e09b1211ebde02c
XristinaSyk <65867825+XristinaSyk@users.noreply.github.com>
2022-02-09 16:10:29 +0200

    Update covidscript.js

--- covidscript.js
+++ covidscript.js
@@ -1,28 +1,34 @@
-async function covidinfo(){
-    let userpositive=document.getElementById('cb1').value;
-    let coviddate= document.getElementById('cdate').value;
+async function covidinfo() {
+    var userpositive = document.getElementById('cb1').value;
+    var coviddate = document.getElementById('cdate').value;
 
-    let covidinfo = {
-        usrpstive: userpositive,
-        covidate: coviddate
-    }
-
-    console.log(covidinfo);
-    if(covidinfo.usrpstive==='on' && covidinfo.covidate!=='')
-    {
-        console.log(covidinfo);
-        alert("Επιτυχής καταχώρηση!");
-        let covresponse = await axios.post('/sendcovidinfo', covidinfo);
-
-        console.log(covresponse);
-    }
-    else if(covidinfo.usrpstive!=='on' || covidinfo.covidate==='')
-    {
+    if (userpositive !== 'on' || coviddate === '') {
         alert("Παρακαλώ συμπληρώστε όλα τα στοιχεία");
+    } else if (userpositive === 'on' && coviddate !== '') {
+        var userpositive = $("#cb1").val();
+        var coviddate = $("#cdate").val();
+
+        $.ajax({
+            url: '/sendcovidinfo', // to path pou eixa mesa sto axios.post
+            method: 'POST',
+            data: { //oti eixa balei mesa sto obj info
+                usrpstive: userpositive,
+                covidate: coviddate
+            },
+            error: function(xhr, status, error) { console.log(status, error); }, //opws einai kopi peist
+            success: function(response) {
+                if (response == 'valid') {
+                    alert("Επιτυχής καταχώρηση!");
+                    window.location.href = 'http://localhost:3000/mainpage';
+                } else if (response == 'invalid') {
+                    alert('Δεν επιτρέπεται η καταχώρηση νέας δήλωσης!')
+                    window.location.href = 'http://localhost:3000/mainpage';
+                }
+            }
+        });
     }
-    //let covresponse = await axios.post('/sendcovidinfo', covidinfo
 }
 
-async function redirected(){
-    window.location.href="/mainpage";
+async function redirected() {
+    window.location.href = 'http://localhost:3000/mainpage';
 }
